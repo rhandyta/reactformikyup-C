651e3c76f10588a6ae93d71847daef1b60782413
Rhandyta Briantama <rhandytabriantama@gmail.com>
2023-01-06 14:32:35 +0700

    isValid & isSubmitting

--- src/components/FormRegister.js
+++ src/components/FormRegister.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Field, Form, Formik, ErrorMessage, FieldArray } from "formik";
 import * as yup from "yup";
 
 function FormRegister() {
     const initialValues = {
         name: "",
-        email: "",
-        motivation: "",
-        gender: "",
-        handphone: [""],
+        // email: "",
+        // motivation: "",
+        // gender: "",
+        // handphone: [""],
         classRoom: "",
         masteredLanguages: [],
     };
 
-    const onSubmit = (values) => {
-        console.log(values);
+    const onSubmit = (values, props) => {
+        setTimeout(() => {
+            props.setSubmitting(false);
+        }, 3000);
     };
 
     const validationSchema = yup.object({
@@ -23,17 +25,17 @@ function FormRegister() {
             .required("Name is required")
             .min(5, "Name must be longer than 5 characters")
             .trim(),
-        email: yup
-            .string()
-            .required("Email is required")
-            .email("Enter e-mail format")
-            .trim(),
-        motivation: yup
-            .string()
-            .required("Motivation is required")
-            .min(25, "Motivation must be longer than 25 characters")
-            .trim(),
-        gender: yup.string().required("Gender is required"),
+        // email: yup
+        //     .string()
+        //     .required("Email is required")
+        //     .email("Enter e-mail format")
+        //     .trim(),
+        // motivation: yup
+        //     .string()
+        //     .required("Motivation is required")
+        //     .min(25, "Motivation must be longer than 25 characters")
+        //     .trim(),
+        // gender: yup.string().required("Gender is required"),
         classRoom: yup.string().required("Class Room is required"),
         masteredLanguages: yup
             .array()
@@ -48,181 +50,198 @@ function FormRegister() {
                 onSubmit={onSubmit}
                 validationSchema={validationSchema}
             >
-                <Form autoComplete="off">
-                    <div className="form-group">
-                        <label htmlFor="name">Name</label>
-                        <Field
-                            type="text"
-                            name="name"
-                            id="name"
-                            placeholder="Name..."
-                            className="input"
-                        />
-                        <ErrorMessage name="name">
-                            {(error) => (
-                                <span className="input-error">{error}</span>
-                            )}
-                        </ErrorMessage>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <Field
-                            type="email"
-                            name="email"
-                            id="email"
-                            placeholder="E-mail..."
-                            className="input"
-                        />
-                        <ErrorMessage name="email">
-                            {(error) => (
-                                <span className="input-error">{error}</span>
-                            )}
-                        </ErrorMessage>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="handphone">Phone Number</label>
-                        <FieldArray name="handphone">
-                            {(props) => {
-                                const { form, push, remove } = props;
-                                const { values } = form;
-                                const { handphone } = values;
-
-                                return handphone.map((number, i) => {
-                                    return (
-                                        <div key={i}>
-                                            <Field
-                                                name={`handphone[${i}]`}
-                                                className="input"
-                                            />
-                                            <button
-                                                type="button"
-                                                style={{ padding: ".3rem" }}
-                                                onClick={() => push("")}
-                                            >
-                                                +
-                                            </button>
-                                            {i > 0 && (
+                {(props) => {
+                    console.log(props.isValid);
+                    return (
+                        <Form autoComplete="off">
+                            <div className="form-group">
+                                <label htmlFor="name">Name</label>
+                                <Field
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    placeholder="Name..."
+                                    className="input"
+                                />
+                                <ErrorMessage name="name">
+                                    {(error) => (
+                                        <span className="input-error">
+                                            {error}
+                                        </span>
+                                    )}
+                                </ErrorMessage>
+                            </div>
+                            {/* <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <Field
+                                type="email"
+                                name="email"
+                                id="email"
+                                placeholder="E-mail..."
+                                className="input"
+                            />
+                            <ErrorMessage name="email">
+                                {(error) => (
+                                    <span className="input-error">{error}</span>
+                                )}
+                            </ErrorMessage>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="handphone">Phone Number</label>
+                            <FieldArray name="handphone">
+                                {(props) => {
+                                    const { form, push, remove } = props;
+                                    const { values } = form;
+                                    const { handphone } = values;
+    
+                                    return handphone.map((number, i) => {
+                                        return (
+                                            <div key={i}>
+                                                <Field
+                                                    name={`handphone[${i}]`}
+                                                    className="input"
+                                                />
                                                 <button
                                                     type="button"
                                                     style={{ padding: ".3rem" }}
-                                                    onClick={() => remove()}
+                                                    onClick={() => push("")}
                                                 >
-                                                    -
+                                                    +
                                                 </button>
-                                            )}
-                                        </div>
-                                    );
-                                });
-                            }}
-                        </FieldArray>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="motivation">Motivation</label>
-                        <Field
-                            as="textarea"
-                            name="motivation"
-                            id="motivation"
-                            cols="30"
-                            rows="10"
-                            placeholder="Motivation..."
-                            className="input"
-                        />
-                        <ErrorMessage name="motivation">
-                            {(error) => (
-                                <span className="input-error">{error}</span>
-                            )}
-                        </ErrorMessage>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="gender">Gender</label>
-                        <Field
-                            as="select"
-                            name="gender"
-                            id="gender"
-                            className="input"
-                        >
-                            <option value="">-- Select Gender --</option>
-                            <option value="Pria">Pria</option>
-                            <option value="Wanita">Wanita</option>
-                        </Field>
-                        <ErrorMessage name="gender">
-                            {(error) => (
-                                <span className="input-error">{error}</span>
-                            )}
-                        </ErrorMessage>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="gender">Class Room</label>
-                        <label htmlFor="ReactJS">
+                                                {i > 0 && (
+                                                    <button
+                                                        type="button"
+                                                        style={{ padding: ".3rem" }}
+                                                        onClick={() => remove()}
+                                                    >
+                                                        -
+                                                    </button>
+                                                )}
+                                            </div>
+                                        );
+                                    });
+                                }}
+                            </FieldArray>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="motivation">Motivation</label>
                             <Field
-                                name="classRoom"
-                                id="ReactJS"
+                                as="textarea"
+                                name="motivation"
+                                id="motivation"
+                                cols="30"
+                                rows="10"
+                                placeholder="Motivation..."
                                 className="input"
-                                value="ReactJS"
-                                type="radio"
                             />
-                            React JS
-                        </label>
-                        <label htmlFor="VueJS">
+                            <ErrorMessage name="motivation">
+                                {(error) => (
+                                    <span className="input-error">{error}</span>
+                                )}
+                            </ErrorMessage>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="gender">Gender</label>
                             <Field
-                                type="radio"
-                                name="classRoom"
-                                id="VueJS"
+                                as="select"
+                                name="gender"
+                                id="gender"
                                 className="input"
-                                value="VueJS"
-                            />
-                            Vue JS
-                        </label>
-                        <ErrorMessage name="classRoom">
-                            {(error) => (
-                                <span className="input-error">{error}</span>
-                            )}
-                        </ErrorMessage>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="masteredLanguages">
-                            Mastered Languages
-                        </label>
-                        <label htmlFor="Java">
-                            <Field
-                                name="masteredLanguages"
-                                id="Java"
-                                className="input"
-                                value="Java"
-                                type="checkbox"
-                            />
-                            Java
-                        </label>
-                        <label htmlFor="PHP">
-                            <Field
-                                type="checkbox"
-                                name="masteredLanguages"
-                                id="PHP"
-                                className="input"
-                                value="PHP"
-                            />
-                            PHP
-                        </label>
-                        <label htmlFor="Python">
-                            <Field
-                                type="checkbox"
-                                name="masteredLanguages"
-                                id="Python"
-                                className="input"
-                                value="Python"
-                            />
-                            Python
-                        </label>
-                        <ErrorMessage name="masteredLanguages">
-                            {(error) => (
-                                <span className="input-error">{error}</span>
-                            )}
-                        </ErrorMessage>
-                    </div>
-                    <button type="submit" className="btn">
-                        Register
-                    </button>
-                </Form>
+                            >
+                                <option value="">-- Select Gender --</option>
+                                <option value="Pria">Pria</option>
+                                <option value="Wanita">Wanita</option>
+                            </Field>
+                            <ErrorMessage name="gender">
+                                {(error) => (
+                                    <span className="input-error">{error}</span>
+                                )}
+                            </ErrorMessage>
+                        </div> */}
+                            <div className="form-group">
+                                <label htmlFor="gender">Class Room</label>
+                                <label htmlFor="ReactJS">
+                                    <Field
+                                        name="classRoom"
+                                        id="ReactJS"
+                                        className="input"
+                                        value="ReactJS"
+                                        type="radio"
+                                    />
+                                    React JS
+                                </label>
+                                <label htmlFor="VueJS">
+                                    <Field
+                                        type="radio"
+                                        name="classRoom"
+                                        id="VueJS"
+                                        className="input"
+                                        value="VueJS"
+                                    />
+                                    Vue JS
+                                </label>
+                                <ErrorMessage name="classRoom">
+                                    {(error) => (
+                                        <span className="input-error">
+                                            {error}
+                                        </span>
+                                    )}
+                                </ErrorMessage>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="masteredLanguages">
+                                    Mastered Languages
+                                </label>
+                                <label htmlFor="Java">
+                                    <Field
+                                        name="masteredLanguages"
+                                        id="Java"
+                                        className="input"
+                                        value="Java"
+                                        type="checkbox"
+                                    />
+                                    Java
+                                </label>
+                                <label htmlFor="PHP">
+                                    <Field
+                                        type="checkbox"
+                                        name="masteredLanguages"
+                                        id="PHP"
+                                        className="input"
+                                        value="PHP"
+                                    />
+                                    PHP
+                                </label>
+                                <label htmlFor="Python">
+                                    <Field
+                                        type="checkbox"
+                                        name="masteredLanguages"
+                                        id="Python"
+                                        className="input"
+                                        value="Python"
+                                    />
+                                    Python
+                                </label>
+                                <ErrorMessage name="masteredLanguages">
+                                    {(error) => (
+                                        <span className="input-error">
+                                            {error}
+                                        </span>
+                                    )}
+                                </ErrorMessage>
+                            </div>
+                            <button
+                                type="submit"
+                                className="btn"
+                                disabled={props.isSubmitting || !props.isValid}
+                            >
+                                {props.isSubmitting
+                                    ? "Please Wait"
+                                    : "Register"}
+                            </button>
+                        </Form>
+                    );
+                }}
             </Formik>
         </div>
     );
